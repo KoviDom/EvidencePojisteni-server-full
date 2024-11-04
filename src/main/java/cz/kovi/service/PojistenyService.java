d2baa167d0be7e9ff8713477ae6322e38241e142
KoviDom <dominik.kovac01@seznam.cz>
2024-11-04 15:56:06 +0100

    pridani u anotace entity

--- src/main/java/cz/kovi/service/PojistenyService.java
+++ src/main/java/cz/kovi/service/PojistenyService.java
@@ -1,4 +1,43 @@
 package cz.kovi.service;
 
+import cz.kovi.dto.PojistenyDTO;
+
+import java.util.List;
+
 public interface PojistenyService {
+
+    /**
+     * pridani Pojistenyho
+     * @param pojistenyDTO
+     * @return pridani novyho Pojistenyho
+     */
+    PojistenyDTO pridatPojisteny(PojistenyDTO pojistenyDTO);
+
+    /**
+     * odstraneni pojistenyho podle ID
+     * @param id
+     */
+    void odstranitPojisteny(long id);
+
+    /**
+     * seznam vsech pojistenych
+     * @return List/seznam vsech pojistenych
+     */
+    List<PojistenyDTO> ziskaniVsechPojistenych();
+
+    /**
+     * pojistenyho, ktery chceme zobrazit a jeho detaily
+     * @param id
+     * @return detail osoby podle ID
+     */
+    PojistenyDTO dostatPojistenyhoPodleId(long id);
+
+    /**
+     * uprava pojistenyho
+     * @param id
+     * @param pojistenyDTO
+     * @return uprava pojistenyho podle ID
+     */
+    PojistenyDTO upravaPojistenyho(long id, PojistenyDTO pojistenyDTO);
+
 }
