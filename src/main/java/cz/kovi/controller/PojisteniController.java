2d732fd5eb2401b4afd32bb77309bcbee0365a4f
KoviDom <dominik.kovac01@seznam.cz>
2024-11-09 01:09:01 +0100

    controller

--- src/main/java/cz/kovi/controller/PojisteniController.java
+++ src/main/java/cz/kovi/controller/PojisteniController.java
@@ -1,4 +1,48 @@
 package cz.kovi.controller;
 
+import cz.kovi.dto.PojisteniDTO;
+import cz.kovi.service.PojisteniService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/pojisteni")
 public class PojisteniController {
+
+    private final PojisteniService pojisteniService;
+
+    @Autowired
+    public PojisteniController(PojisteniService pojisteniService) {
+        this.pojisteniService = pojisteniService;
+    }
+
+    @PostMapping("/novy")
+    public PojisteniDTO pridatPojisteni(@RequestBody PojisteniDTO pojisteniDTO) {
+        return pojisteniService.pridatPojisteni(pojisteniDTO);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void smazatPojisteni(@PathVariable long id) {
+        pojisteniService.smazatPojisteni(id);
+    }
+
+    @GetMapping("")
+    public List<PojisteniDTO> seznamVsechPojistenich() {
+        return pojisteniService.seznamVsechPojistenich();
+    }
+
+    @GetMapping("/detail/{id}")
+    public PojisteniDTO dostatPojistenihoPodleId(@PathVariable long id) {
+        return pojisteniService.dostatPojistenihoPodleId(id);
+    }
+
+    @PutMapping("{id}")
+    public PojisteniDTO upravaPojisteniho(@PathVariable long id, @RequestBody PojisteniDTO pojisteniDTO) {
+        return pojisteniService.upravaPojisteniho(id, pojisteniDTO);
+    }
+
 }
--- src/main/java/cz/kovi/controller/PojistenyController.java
+++ src/main/java/cz/kovi/controller/PojistenyController.java
@@ -1,4 +1,48 @@
 package cz.kovi.controller;
 
+import cz.kovi.dto.PojistenyDTO;
+import cz.kovi.service.PojistenyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pojistenci")
 public class PojistenyController {
+
+    private final PojistenyService pojistenyService;
+
+    @Autowired
+    public PojistenyController(PojistenyService pojistenyService) {
+        this.pojistenyService = pojistenyService;
+    }
+
+    @PostMapping("/novy")
+    public PojistenyDTO pridatPojisteny(@RequestBody PojistenyDTO pojistenyDTO) {
+        return pojistenyService.pridatPojisteny(pojistenyDTO);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void smazatPojisteny(@PathVariable long id) {
+        pojistenyService.smazatPojisteny(id);
+    }
+
+    @GetMapping("")
+    public List<PojistenyDTO> seznamVsechPojistenych() {
+        return pojistenyService.seznamVsechPojistenych();
+    }
+
+    @GetMapping("/detail/{id}")
+    public PojistenyDTO dostatPojistenyhoPodleId(@PathVariable long id) {
+        return pojistenyService.dostatPojistenyhoPodleId(id);
+    }
+
+    @PutMapping("{id}")
+    public PojistenyDTO upravaPojistenyho(@PathVariable long id, @RequestBody PojistenyDTO pojistenyDTO) {
+        return pojistenyService.upravaPojistenyho(id, pojistenyDTO);
+    }
+
 }
